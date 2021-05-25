a7f387a748c573587e43b7be3dcf2860e3121b80
Oana Cretu <oan.max@gmail.com>
2021-05-25 06:10:35 -0700

    characters

---
+++ C/Playing With Characters/Main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+	//take a character, a string and a sentence as input
+    char ch;
+    char s[100];
+    char sen[100];
+    scanf("%c", &ch);
+    scanf("\n");
+    scanf("%[^\n]%*c", &s);
+    scanf("%[^\n]%*c", &sen);
+    //print the character, the string, and the sentence as output
+    printf("%c\n%s\n%s\n", ch, s, sen);
+    return 0;
+}
