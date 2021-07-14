6b62f735c30a725c37f74384f0b604712ec91d38
oanmaxx <oan.max@gmail.com>
2021-07-14 15:11:55 +0300

    add up integers of an array

---
+++ Algorithms/Warmup/Solution.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * Task: Given an array of integers, find the sum of its elements.
+ * Example:
+ *      array arr = [1,2,3]     => 1+2+3=6
+ */
+
+class Result {
+    /*
+     * Complete the 'simpleArraySum' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY ar as parameter.
+     */
+
+    public static int simpleArraySum(List<Integer> ar) {
+        // Write your code here
+        int sum = 0;
+        //iterate through List of integers
+        for (int number : ar) {
+            //add up each element of the list
+           sum += number;
+        }
+        return sum;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] arTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> ar = new ArrayList<>();
+
+        for (int i = 0; i < arCount; i++) {
+            int arItem = Integer.parseInt(arTemp[i]);
+            ar.add(arItem);
+        }
+
+        int result = Result.simpleArraySum(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
