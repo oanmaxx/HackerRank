52044c603f72ac33f8984957064a94896f7bd737
oanmaxx <oan.max@gmail.com>
2021-10-07 16:09:01 +0300

    sum, difference, product

---
+++ Python/Introduction/Arithmetic Operators/main.py
@@ -0,0 +1,11 @@
+# Task: https://www.hackerrank.com/challenges/python-arithmetic-operators/problem
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    # sum
+    print(a + b)
+    # difference
+    print(a - b)
+    # product
+    print(a * b)
