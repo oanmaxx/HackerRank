f2cf99a9b87d79d349673516dd62d8af50fc5ddc
oanmaxx <oan.max@gmail.com>
2021-10-08 11:41:20 +0300

    leap year

---
+++ Python/Introduction/Write a function/main.py
@@ -0,0 +1,12 @@
+# Task: https://www.hackerrank.com/challenges/write-a-function/problem
+
+def is_leap(year):
+    leap = False
+    # Write your logic here
+    if year % 400 == 0 or (year % 4 == 0) and (year % 100 != 0):
+        leap = True
+    return leap
+
+
+year = int(input())
+print(is_leap(year))
