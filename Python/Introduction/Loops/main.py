a61c8b8a9bea4f920e0318f61bd280b745e4736a
oanmaxx <33283843+oanmaxx@users.noreply.github.com>
2021-12-07 11:46:38 +0200

    Removed Python folder

--- Python/Introduction/Loops/main.py
+++
@@ -1,9 +0,0 @@
-# Task: https://www.hackerrank.com/challenges/python-loops/problem
-
-if __name__ == '__main__':
-    n = int(input())
-    if 1 <= n <= 20:
-        i = 0
-        while i < n:
-            print(i ** 2)
-            i += 1
